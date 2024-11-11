67c681cbe72fc85b824c961c86117909c7f4564f
pabalcber <pabloalcantarabernal@gmail.com>
2024-11-11 19:17:13 +0100

    Feat: Homepage

--- backend/myapp/views.py
+++ backend/myapp/views.py
@@ -1,22 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
-from rest_framework import Response
+from rest_framework.response import Response  # Solo una vez
 from myapp.models import Product
 from rest_framework.viewsets import ModelViewSet
 from myapp.serializer import *
 from rest_framework.authtoken.models import Token
 from django.contrib.auth import authenticate, login
 from rest_framework.views import APIView
-from rest_framework.response import Response
 from rest_framework import status, permissions
-from rest_framework.response import Response
-from rest_framework import status, permissions
-from rest_framework.authtoken.models import Token
-from rest_framework.views import APIView
-from myapp.serializer import UserSerializer,LoginSerializer
-from rest_framework.permissions import IsAuthenticated 
 from drf_spectacular.utils import extend_schema, OpenApiResponse, inline_serializer
-from rest_framework.views import APIView
 from django.core.mail import send_mail
 from django.template.loader import render_to_string
 from django.utils.html import strip_tags
@@ -24,7 +16,16 @@ from drf_spectacular.utils import OpenApiParameter
 from rest_framework import generics
 
 
-#Devuelve el listado de productos
+def index(request: HttpRequest) -> HttpResponse:
+    return HttpResponse("PGPI Grupo 2.7")
+
+def home(request):
+    return render(request, 'home.html')
+
+def catalogue(request):
+    return render(request, 'catalogue.html')
+
+# Devuelve el listado de productos
 class ProductListAPIView(generics.ListAPIView):
     serializer_class = ProductSerializer
     queryset = Product.objects.all()
@@ -53,5 +54,3 @@ class ProductDetailView(APIView):
             return Response(serializer.data, status=status.HTTP_200_OK)
         except Product.DoesNotExist:
             return Response(status=status.HTTP_404_NOT_FOUND)
-
-
